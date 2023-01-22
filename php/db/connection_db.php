9c4317e9dd94adbf14f3bb7eb1df1d4967acc518
Santiago Albuja <salbuja@cotopaxi.k12.ec>
2023-01-22 13:20:27 -0500

    Actor controller v2

--- php/db/connection_db.php
+++ php/db/connection_db.php
@@ -1,4 +1,21 @@
 <?php
+
+function OpenConn(){
+  $host = 'localhost';
+  $user = 'root';   
+  $pass = "";   
+  $database = 'actividad1';
+  $conn = mysqli_connect($host,$user,$pass,$database);   
+  if (!$conn) {                                             
+      die("Connection failed: " . mysqli_connect_error());     
+  }
+  return $conn;
+}
+
+function CloseConn($conn){
+  $conn -> close();
+}
+
 function initConnectionDb(){
   $dbhost = 'localhost';
   $dbuser = 'root';   
---
+++ php/views/actors/delete.php
@@ -0,0 +1,12 @@
+<?php  include "header.php" ?>
+<?php 
+     require_once('../controllers/serie_controller.php');
+     echo 'ingresa a borrar';
+     if(isset($_GET['delete']))
+     {
+         $serie_id= $_GET['delete'];
+         deleteSerie($serie_id);
+         header("Location: series.php");
+     }
+    ?>
+<?php include "footer.php" ?>
