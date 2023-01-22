ec5dd40b280b07215dc345692e0f15e3d2a2ec13
Santiago Albuja <salbuja@cotopaxi.k12.ec>
2023-01-22 13:20:27 -0500

    Actor controller v2

--- php/controllers/actor_controller.php
+++ php/controllers/actor_controller.php
@@ -1,30 +1,28 @@
 <?php
-    require_once('../../php/models/actor.php');
+    require_once('/php/models/actor.php');
     require_once('../../php/db/connection_db.php');
 
-    function listActors() {
-    
-    
-    $model = new Actor() ;
-    $actorList = $model->getAll();
-    $actorObjectArray = [];
-    
-    foreach ($actorList as $actorItem) {
-    $actorObject = new Actor($actorItem->getId(), $actorItem->getfirstname(),$actorItem->getlastname(), $actorItem->getDOB(),$actorItem->getidcountry());
-    array_push( $actorObjectArray, $actorObject) ;
+    function listActors()
+    {    
+        $model = new Actor() ;
+        $actorList = $model->getAll();
+        $actorObjectArray = [];
+        
+        foreach ($actorList as $actorItem) {
+        $actorObject = new Actor($actorItem->getId(), $actorItem->getfirstname(),$actorItem->getlastname(), $actorItem->getDOB(),$actorItem->getidcountry());
+        array_push( $actorObjectArray, $actorObject) ;
+        }
+        return $actorObjectArray;
     }
-    return $actorObjectArray;
-}
 
-    public function getAll( )
+    function getAll()
     {
-
-    $mysqli = $this->initConnectionDb() ;
-    $query = $mysqli->query("SELECT * FROM actors") ;
-    $listData = [];
-    foreach ($query as $item) {
-    $itemObject = new Actor($item['id'], $item['firstname'], $item['lastname'], $item['DOB'], $item['idcountry'] ) ;
-    array_push( $listData, $itemObject) ;
+        $mysqli = $this->initConnectionDb() ;
+        $query = $mysqli->query("SELECT * FROM actors") ;
+        $listData = [];
+        foreach ($query as $item) {
+        $itemObject = new Actor($item['id'], $item['firstname'], $item['lastname'], $item['DOB'], $item['idcountry'] ) ;
+        array_push( $listData, $itemObject) ;
     }
     $mysqli ->close( ) ;
     return $listData;
---
+++ php/views/actors/footer.php
@@ -0,0 +1,3 @@
+<footer class="blockquote-footer fixed-bottom">Universidad Internacional de Valencia <cite title="Source Title"><a href="https://codewithbish.com/free-projects-download/" target="_blank">www.universidadviu.com</a></cite></footer>
+</body>
+</html>
